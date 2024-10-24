0fb18575f640c085c330e5e1f8579c831642719d
soheekimdev <soheekim.dev@gmail.com>
2024-10-24 17:14:47 +0900

    feat: css 코드를 tailwindcss로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import "./App.css";
-import { contents } from "./assets/data/data";
-import Content from "./components/Content";
-import Header from "./components/Header";
+import { contents } from './assets/data/data';
+import Content from './components/Content';
+import Header from './components/Header';
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <section className="flex flex-wrap justify-center gap-5 py-5 px-10">
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
+function Li({ text }) {
+  return <li className="list-none font-normal text-[13px]">{text}</li>;
+}
+
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className="flex justify-between items-center py-2.5 px-5 bg-black">
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+      <ul className="flex justify-center items-center gap-5">
+        <Li text="로그인" />
+        <Li text="회원가입" />
+        <Li text="내클래스" />
       </ul>
     </header>
   );
